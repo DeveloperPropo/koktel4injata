4839f610940714f830784da4f5b15c5fe885f54e
developerpropo <developerprop@gmail.com>
2021-10-23 23:39:08 +0200

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        aspernatur commodi, in libero vitae eligendi quo saepe voluptates
+        officiis, tenetur deserunt nostrum amet, aliquam excepturi temporibus
+        labore aliquid iusto et adipisci aut. Veritatis voluptas facere omnis
+        sint nobis, dolorum, dolore pariatur accusantium, perspiciatis expedita
+        molestias! Similique suscipit nisi enim consequatur?
+      </p>
+    </section>
+  );
+};
+
+export default About;
